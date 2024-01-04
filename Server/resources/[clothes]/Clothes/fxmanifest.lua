5fd7ca0dc8ca47819fc2e874bbd3cdf1a7c32678
Peoplecallmedre <dremonteiro15@gmail.com>
2024-01-04 21:27:57 +0100

    clothes

---
+++ Server/resources/[clothes]/Clothes/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "bodacious"
+game "gta5"
+
+files {
+	"data/mp_f_freemode_01_mp_f_female_one.meta",
+	"data/mp_f_freemode_01_mp_f_female_two.meta",
+	"data/mp_m_freemode_01_mp_m_male_one.meta"
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_female_one.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_female_two.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_male_one.meta"
