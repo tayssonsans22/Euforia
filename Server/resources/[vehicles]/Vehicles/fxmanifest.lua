6118744f7217d4f2c72a2395400fe5a120419cf6
Peoplecallmedre <dremonteiro15@gmail.com>
2024-01-04 21:14:13 +0100

    Vehicles

---
+++ Server/resources/[vehicles]/Vehicles/fxmanifest.lua
@@ -0,0 +1,117 @@
+fx_version "bodacious"
+game "gta5"
+
+files {
+	"data/*",
+	"stream/*",
+	"stream/**/*",
+	"stream/**/**/*",
+	"audio/*",
+	"audio/**/*",
+	"audio/**/**/*",
+	"popgroups.ymt",
+	"popcycle.dat"
+}
+
+data_file "DLC_POP_GROUPS" "popgroups.ymt"
+data_file "POPSCHED_FILE" "popcycle.dat"
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicleaihandlinginfo.meta"
+data_file "CARCOLS_GEN9_FILE" "data/carcols_gen9.meta"
+data_file "CARMODCOLS_GEN9_FILE" "data/carmodcols_gen9.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
+data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/fordvoodoo/dlc_fordvoodoo"
+data_file "AUDIO_GAMEDATA" "audio/fordvoodoo/fordvoodoo_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/fordvoodoo/fordvoodoo_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/s85b50/dlc_s85b50"
+data_file "AUDIO_GAMEDATA" "audio/s85b50/s85b50_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s85b50/s85b50_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/ea888/dlc_ea888"
+data_file "AUDIO_GAMEDATA" "audio/ea888/ea888_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/ea888/ea888_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/sultanrsv8/dlc_v8sultanrs"
+data_file "AUDIO_GAMEDATA" "audio/sultanrsv8/sultanrsv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/sultanrsv8/sultanrsv8_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/s15sound/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/s15sound/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/s15sound/s15sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/n4g63t/dlc_n4g63t"
+data_file "AUDIO_GAMEDATA" "audio/n4g63t/n4g63t_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/n4g63t/n4g63t_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/gt3rstun/dlc_gt3rstun"
+data_file "AUDIO_SYNTHDATA" "audio/gt3rstun/gt3rstun_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/gt3rstun/gt3rstun_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/gt3rstun/gt3rstun_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/trumpetzr/dlc_trumpetzr"
+data_file "AUDIO_GAMEDATA" "audio/trumpetzr/trumpetzrc_game.dat"
+data_file "AUDIO_GAMEDATA" "audio/trumpetzr/trumpetzr_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/trumpetzr/trumpetzrc_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audio/trumpetzr/trumpetzr_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/f10m5/dlc_f10m5"
+data_file "AUDIO_GAMEDATA" "audio/f10m5/f10m5_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/f10m5/f10m5_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/sentinelsg4/dlc_sentinelsg4"
+data_file "AUDIO_GAMEDATA" "audio/sentinelsg4/sentinelsg4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/sentinelsg4/sentinelsg4_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/subaruej20/dlc_subaruej20"
+data_file "AUDIO_GAMEDATA" "audio/subaruej20/subaruej20_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/subaruej20/subaruej20_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/rotary7/dlc_rotary7"
+data_file "AUDIO_GAMEDATA" "audio/rotary7/rotary7_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/rotary7/rotary7_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/f20c/dlc_f20c"
+data_file "AUDIO_GAMEDATA" "audio/f20c/f20c_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/f20c/f20c_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/ftypesound/dlc_ftypesound"
+data_file "AUDIO_GAMEDATA" "audio/ftypesound/ftypesound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/ftypesound/ftypesound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/majimalm/dlc_majimagt"
+data_file "AUDIO_GAMEDATA" "audio/majimalm/majimalm_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/majimalm/majimalm_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/ta488f154/dlc_ta488f154"
+data_file "AUDIO_GAMEDATA" "audio/ta488f154/ta488f154_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/ta488f154/ta488f154_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/npolchar/dlc_npolchar"
+data_file "AUDIO_GAMEDATA" "audio/npolchar/npolchar_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/npolchar/npolchar_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/cvpiv8/dlc_cvpiv8"
+data_file "AUDIO_GAMEDATA" "audio/cvpiv8/cvpiv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/cvpiv8/cvpiv8_sounds.dat"
